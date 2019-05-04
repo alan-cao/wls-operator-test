13a4aef4d59aa34d162da698f71cffe938862c4a
alan-cao <cao_yulong@hotmail.com>
2019-05-04 21:43:22 +0000

    update

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,3 +1,8 @@
-TEST_DOMAIN=sample-domain21
+TEST_DOMAIN=sample-domain21new
 WLS_OPERATOR_HOME=/home/opc/github/oracle/weblogic-kubernetes-operator.master
+WEBLOGIC_OPERATOR_IMAGE="oracle/weblogic-kubernetes-operator:2.1"
+
+export TEST_DOMAIN
+export WLS_OPERATOR_HOME
+export WEBLOGIC_OPERATOR_IMAGE
 
