43dc7763f603c3fb61d3beb465a92e51a7d9c421
alan-cao <cao_yulong@hotmail.com>
2019-05-10 06:04:33 +0000

    update

---
+++ scripts/config-fmw-domain.sh
@@ -0,0 +1,61 @@
+TEST_DIR=./outputs
+
+mkdir $TEST_DIR
+
+echo $TEST_DOMAIN
+echo $TEST_DOMAIN_NS
+kubectl create namespace $TEST_DOMAIN_NS
+
+helm upgrade \
+  --reuse-values \
+  --set "domainNamespaces={${TEST_DOMAIN_NS}}" \
+  --wait \
+  sample-weblogic-operator \
+  ${WLS_OPERATOR_HOME}/kubernetes/charts/weblogic-operator
+
+
+helm upgrade \
+  --reuse-values \
+  --set "kubernetes.namespaces={traefik,${TEST_DOMAIN_NS}}" \
+  --wait \
+  traefik-operator \
+  stable/traefik
+
+# create weblogic credential
+$WLS_OPERATOR_HOME/kubernetes/samples/scripts/create-weblogic-domain-credentials/create-weblogic-credentials.sh \
+  -u weblogic -p welcome1 -n $TEST_DOMAIN_NS -d $TEST_DOMAIN
+
+
+# create RCU credentials
+$WLS_OPERATOR_HOME/kubernetes/samples/scripts/create-rcu-credentials/create-rcu-credentials.sh -a sys -q Oradoc_db1 -u fmwdomain_wls -p welcome1 -d ${TEST_DOMAIN} -s ${TEST_DOMAIN}-rcu-credentials -n ${TEST_DOMAIN_NS}
+
+
+cp $WLS_OPERATOR_HOME/kubernetes/samples/scripts/create-fmw-infrastructure-domain/create-domain-inputs.yaml ${TEST_DIR}/${TEST_DOMAIN}.yaml
+
+sed -i -e "s/namespace: default/namespace: ${TEST_DOMAIN_NS}/g" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+sed -i -e "s/domainUID: domain1/domainUID: ${TEST_DOMAIN}/g" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+sed -i -e "s/weblogicCredentialsSecretName: domain1-weblogic-credentials/weblogicCredentialsSecretName: ${TEST_DOMAIN}-weblogic-credentials/g" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+sed -i -e "s/persistentVolumeClaimName: domain1-weblogic-sample-pvc/persistentVolumeClaimName: weblogic-sample-pvc/g" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+sed -i -e "s/domain1/${TEST_DOMAIN}/g" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+sed -i -e "s;database:1521/service;10.0.0.2:1521/orclcdb;" ${TEST_DIR}/${TEST_DOMAIN}.yaml
+
+echo "running: $WLS_OPERATOR_HOME/kubernetes/samples/scripts/create-weblogic-domain/domain-home-on-pv/create-domain.sh -i ${TEST_DIR}/${TEST_DOMAIN}.yaml -o $TEST_DIR/directory -e"
+$WLS_OPERATOR_HOME/kubernetes/samples/scripts/create-fmw-infrastructure-domain/create-domain.sh -i ${TEST_DIR}/${TEST_DOMAIN}.yaml -o $TEST_DIR/directory  -e
+
+
+kubectl describe domain $TEST_DOMAIN -n $TEST_DOMAIN_NS 
+
+kubectl get pod -n $TEST_DOMAIN_NS 
+
+kubectl get svc -n $TEST_DOMAIN_NS
+
+
+helm install ${WLS_OPERATOR_HOME}/kubernetes/samples/charts/ingress-per-domain \
+  --name ${TEST_DOMAIN}-ingress \
+  --namespace ${TEST_DOMAIN_NS} \
+  --set wlsDomain.domainUID=${TEST_DOMAIN} \
+  --set traefik.hostname=${TEST_DOMAIN}.org
+
+
+kubectl get pod -n $TEST_DOMAIN_NS
+
